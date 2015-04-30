2da3483cbcfd75ac88c89e643936b86eacfcde8d
Ivan Kuznetsov <me@jeiwan.ru>
2015-04-30 13:02:11 +0300

    Another try

--- lib/synchro.rb
+++ lib/synchro.rb
@@ -1,4 +1,17 @@
-require "synchro/synchronize"
-
 module Synchro
+  def synchro
+    after_create :synchro_synchronize
+
+    include InstanceMethods
+  end
+
+  module InstanceMethods
+    def synchro_synchronize
+      Rails.logger.info ">>>>>>>>>>>>>>>>>>>>>>> YAY <<<<<<<<<<<<<<<<<<<<<<<<"
+    end
+
+    private :synchro_synchronize
+  end
 end
+
+ActiveRecord::Base.send :extend, Synchro
